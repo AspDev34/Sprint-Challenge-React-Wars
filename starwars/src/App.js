01e8d62c926c00ee7f7d1b33d08eded9b9c31d9b
Ike Otu <ikeotu@msn.com>
2020-01-24 16:08:01 -0800

    mvp reached. component made with styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import CharacterComp from './components/myComponent';
+import { Badge } from 'reactstrap';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,24 +12,46 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  // const [data] = useState(data)
+  const [data, setData] = useState([])
 
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/1/')
+      .get('https://swapi.co/api/people/')
          .then(res => {
-           console.log(res)
+           setData(res.data.results)
          })
          .catch(error => {
            console.log("Sorry nothing returned", error);
          })
   }, []); 
-
+  console.log(data)
+  
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header"><Badge color="primary">React Wars</Badge></h1>
+
+    {data.map(character => {
+      return(
+      // <div>  
+      //   <p>{character.name}</p>
+      //   <p>{character.mass}</p>
+      // </div>
+      <CharacterComp characterName={character.name} characterMass={character.mass}/>
+      )
+    })}
+
+
+
+
     </div>
+
+    
+
+    
+
   );
+
+  
 }
 
 export default App;
